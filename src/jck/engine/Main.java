7ab208e8dc21126ba1bcde4edad02ef0dcf3a7ff
Casper <casperklinkhamer@hotmail.com>
2018-11-13 11:54:01 +0100

    Main class desc update

--- src/jck/engine/Main.java
+++ src/jck/engine/Main.java
@@ -1,8 +1,8 @@
 package jck.engine;
 
 /*
-
-
+    @author Casper Klinkhamer
+    This is the main class for the engine project. The gameloop is at a different location because that doesn't fit in here.
 
  */
 
