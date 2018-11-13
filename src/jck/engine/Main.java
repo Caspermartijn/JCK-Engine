3815442ea532cac358dbfcfcd0fdae004c12ae8c
Casper <casperklinkhamer@hotmail.com>
2018-11-13 23:44:40 +0100

    Added the display and mouse classes and added some context to certain methods

--- src/jck/engine/Main.java
+++ src/jck/engine/Main.java
@@ -15,8 +15,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("version: " + VERSION);
 
-        GameLoop loop = new GameLoop();
-
+        new GameLoop();
     }
 
 }
